94d939f277b3ce49c3ec3fd47552d13572e2b328
AmandaKline <mandyhappypeace@gmail.com>
2017-05-06 15:20:47 -0400

    Appearance edits from jsfiddle

--- WebContent/script.js
+++ WebContent/script.js
@@ -1,31 +1,25 @@
-function getGreeting(d)
-{
-    var greeting = new String();
-    var hour = d.getHours();
-    
-    if (hour < 5){
-        greeting = "Hello Night Owl";
-    }
-    else if (hour < 12){
-        greeting = "Good Morning";
-    }
-    else if (hour < 17){
-        greeting = "Good Afternoon";
-    }
-    else if (hour < 22){
-        greeting = "Good Evening";
-    }
-    else{
-        greeting = "Hello Night Owl";
-    }
-    
-    return greeting;
+function getGreeting(d) {
+  var greeting = new String();
+  var hour = d.getHours();
+
+  if (hour < 5) {
+    greeting = "Hello Night Owl";
+  } else if (hour < 12) {
+    greeting = "Good Morning";
+  } else if (hour < 17) {
+    greeting = "Good Afternoon";
+  } else if (hour < 22) {
+    greeting = "Good Evening";
+  } else {
+    greeting = "Hello Night Owl";
+  }
+
+  return greeting;
 }
 
 window.onload = function() {
-    var date = new Date();
-    greeting = getGreeting(date);
-    
-    var header = document.getElementById("greeting");
-    header.innerHTML = greeting;
+  var date = new Date();
+  greeting = getGreeting(date);
+
+  document.getElementById("greeting").innerHTML= greeting;
 }
